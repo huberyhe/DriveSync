0e0aadee490d235ccede451dda7b517a1e1b9263
Hubery <hyc171819@gmail.com>
2019-05-04 13:10:43 +0800

    dev

--- drive_sync.py
+++ drive_sync.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import os
 import sys
 import time
@@ -7,14 +9,15 @@ import shutil
 import filecmp
 from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler, FileSystemEventHandler
-from multiprocessing import Process
-import threading,Queue
+from multiprocessing import Process,Queue
+import threading
 
 path_one = "/mnt/e/OCOD/test_d1"
 path_another = "/mnt/e/OCOD/test_d2"
 paths = [path_one + '/d1', path_one + '/d2', path_one + '/d3', path_another + '/d1', path_another + '/d2', path_another + '/d3']
 file_handler_stat = {}
 HANDLER_WAIT = 10
+WORKER_NUM = 3
 
 def get_another_path(src_path):
     if path_one in src_path:
@@ -24,7 +27,7 @@ def get_another_path(src_path):
     else:
         return None
 
-def add_handle_queue(event):
+def handle_event_queue(event):
     src_path = event.src_path
     event_type = event.event_type
     is_directory = event.is_directory
@@ -32,7 +35,8 @@ def add_handle_queue(event):
     if not is_directory and (src_path not in file_handler_stat or time.time() - file_handler_stat[src_path] > HANDLER_WAIT):
         dst_path = get_another_path(src_path)
         logging.info("%s %s handle" % (src_path, event_type))
-        time.sleep(HANDLER_WAIT) #延迟处理
+        #延迟处理
+        time.sleep(HANDLER_WAIT)
         try:
             if event_type in ['created']:
                 dst_path_dir = os.path.dirname(dst_path)
@@ -56,14 +60,45 @@ def add_handle_queue(event):
 
 
 class SyncHandler(FileSystemEventHandler):
-    def on_any_event(self, event):
-        add_handle_queue(event)
+    def __init__(self, q):
+        FileSystemEventHandler.__init__(self)
+        self.e_queue = q
 
-def event_handler_p():
+    def on_any_event(self, event):
+        logging.info('queue put')
+        self.e_queue.put(event)
+
+def event_handler_t(nloop, event):
+    '''
+    对文件修改事件的处理线程
+    '''
+    logging.info('in event_handler_t %d' % nloop)
+    handle_event_queue(event)
+
+def event_handler_p(q):
     '''
     对文件修改事件的处理进程，使用多个线程进行处理
     '''
+    workers = []
+    nloops = range(WORKER_NUM)
+    for i in nloops:
+        worker = threading.Thread(target=event_handler_t, args=(i, q,))
+        # worker.setDaemon(True)
+        workers.append(worker)
+
+    for i in nloops:
+        workers[i].start
+
+    while True:
+        if not q.empty():
+            event = q.get()
+            logging.info('queue get')
+            handle_event_queue(event)
+        else:
+            time.sleep(1)
     
+    for i in nloops:
+        workers[i].join()
 
 def file_watchdog_p():
     '''
@@ -73,14 +108,14 @@ def file_watchdog_p():
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(message)s',
+                        format='%(asctime)s - %(funcName)s:%(lineno)d - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
     #启动一个进程，用于处理事件队列
-    q = Queue.Queue()
+    q = Queue()
     p = Process(target=event_handler_p, args=(q,))
     p.start()
     #启动文件看门狗
-    event_handler = SyncHandler()
+    event_handler = SyncHandler(q)
     observers = []
     observer = Observer()
     for path in paths:
